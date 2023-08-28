6bec7f7135d24e6e58b3261ba73fd3c5ca4b7de1
Nermin <nrmeen.ra.123@gmail.com>
2023-08-28 19:00:51 +0300

    tests on inserting employee + improving user experiense

--- addEmployee.php
+++ addEmployee.php
@@ -1,20 +1,20 @@
 <?php
-    if (empty($_POST["fullName"])) {
-        die("fullName is required");
-    }
-    if (empty($_POST["job"])) {
-        die("job is required");
-    }
-    if (empty($_POST["startDate"])) {
-        die("startDate is required");
-    }
-    if (empty($_POST["Gender"])) {
-        die("Gender is required");
-    }
 
+session_start();
 
-    //$password_hash = password_hash($_POST["floatingPassword"], PASSWORD_DEFAULT);
+//if (!isset($_SESSION['loaded'])) {
+  //  $_SESSION['loaded'] = true;
+//}
 
+$errorMessage = "";
+$successMessage = "";
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    if (empty($_POST["fullName"]) || empty($_POST["job"]) || empty($_POST["startDate"]) || empty($_POST["Gender"]) || empty($_POST["daySalary"])) {
+        $errorMessage = "כל השדות חובה";
+
+    } elseif (!is_numeric($_POST["daySalary"])) {
+        $errorMessage = "המשכורת חייבת להיות מספר";
+    } else{
     $fullName = $_POST['fullName'];
     $job = $_POST['job'];
     $startDate = $_POST['startDate'];
@@ -25,17 +25,340 @@
 
     $stmt = $conn->prepare("insert into employee(fullName, job, startDate, Gender, salary) values(?, ?, ?, ?, ?)");
     $stmt->bind_param("ssssd", $fullName, $job, $startDate, $Gender, $daySalary);
-    $execval = $stmt->execute();
-    if($execval){
-        echo "Adding successfully...";
-    } else {
-        die($conn->error . " " . $conn->errno);
+    try {
+        $execval = $stmt->execute();
+        if ($execval) {
+            $successMessage = "העובד נקלט בהצלחה";
+            // You can optionally refresh the page after successful insertion
+            //echo '<meta http-equiv="refresh" content="2">';
+        }
+    } catch (mysqli_sql_exception $e) {
+        if ($e->getCode() === 1062) { // Error code for duplicate entry
+            $errorMessage = "כנראה שהעובד כבר קיים במערכת";
+        } else {
+            $errorMessage = "Error: " . $e->getMessage();
+        }
     }
-    echo $execval;
     $stmt->close();
     $conn->close();
 
+    }
+
+      // Store the messages in session variables
+    $_SESSION["successMessage"] = $successMessage;
+    $_SESSION["errorMessage"] = $errorMessage;
+
+    // Redirect to the same page to prevent re-submission
+    header("Location: " . $_SERVER['REQUEST_URI']);
+    exit();
   
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <title>DASHMIN - Bootstrap Admin Template</title>
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <meta content="" name="keywords">
+    <meta content="" name="description">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="//cdn.jsdelivr.net/npm/alertifyjs@1.13.1/build/css/alertify.min.css"/>
+
+
+    <!-- Favicon -->
+    <link href="img/favicon.ico" rel="icon">
+
+    <!-- Google Web Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Heebo:wght@400;500;600;700&display=swap" rel="stylesheet">
+    
+    <!-- Icon Font Stylesheet -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet">
+
+    <!-- Libraries Stylesheet -->
+    <link href="lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
+    <link href="lib/tempusdominus/css/tempusdominus-bootstrap-4.min.css" rel="stylesheet" />
+
+    <!-- Customized Bootstrap Stylesheet -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Template Stylesheet -->
+    <link href="css/style.css" rel="stylesheet">
+
+    <style>
+    .custom-form {
+        display: flex;
+        justify-content: center;
+    }
+
+    .custom-form-container {
+        max-width: 500px;
+        width: 100%;
+        direction: rtl;
+        text-align: right;
+    }
+
+    .custom-form .form-floating {
+        text-align: right;
+    }
+</style>
+
+   
+</head>
 
 
 
+<body>
+    <div class="container-xxl position-relative bg-white d-flex p-0">
+        <!-- Spinner Start -->
+        <div id="spinner" class="show bg-white position-fixed translate-middle w-100 vh-100 top-50 start-50 d-flex align-items-center justify-content-center">
+            <div class="spinner-border text-primary" style="width: 3rem; height: 3rem;" role="status">
+                <span class="sr-only">Loading...</span>
+            </div>
+        </div>
+        <!-- Spinner End -->
+
+
+         <!-- Sidebar Start -->
+         <div class="sidebar pe-4 pb-3" >
+            <nav class="navbar bg-light navbar-light" >
+                <a href="index.html" class="navbar-brand mx-4 mb-3">
+                    <h3 class="text-primary">אבו רפיק גבארין</h3>
+                    <h3 class="text-primary"><i class="fa fa-hashtag me-2"></i>BUILD-TECH</h3>
+                </a>
+                <div class="d-flex align-items-center ms-4 mb-4">
+                    <div class="position-relative">
+                        <img class="rounded-circle" src="img/user.jpg" alt="" style="width: 40px; height: 40px;">
+                        <div class="bg-success rounded-circle border border-2 border-white position-absolute end-0 bottom-0 p-1"></div>
+                    </div>
+                    <div class="ms-3">
+                        <h6 class="mb-0">רפיק גבארין</h6>
+                        <span>מנהל ראשי</span>
+                    </div>
+                </div>
+                <div class="navbar-nav w-100">
+                    <a href="home.php" class="nav-item nav-link "><i class="fa fa-home me-2"></i>ראשי</a>
+                    <a href="projectsTable.php" class="nav-item nav-link"><i class="fa fa-map me-2"></i>פרויקטים</a>
+                    <a href="bid.html" class="nav-item nav-link"><i class="fa fa-superscript"></i>הצעת מחיר</a>
+                    <a href="economic.php" class="nav-item nav-link"><i class="fa fa-university me-2"></i>כלכלי</a>
+                    <a href="inventory.php" class="nav-item nav-link"><i class="fa fa-cubes me-2"></i>מחסן</a>
+                    <a href="addShift.html" class="nav-item nav-link"><i class="fa fa-book me-2"></i>דיווח משמרת</a>
+                    <a href="reports.php" class="nav-item nav-link"><i class="far fa-file-alt me-2 me-2"></i>דוחות</a>
+                    <div class="nav-item dropdown">
+                        <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown"><i class="fa fa-plus-square me-2"></i>הוספה</a>
+                        <div class="dropdown-menu bg-transparent border-0">
+                        <a href="addEmployee.html" class="dropdown-item active">עובד</a>
+                            <a href="addClient.html" class="dropdown-item">לקוח</a>
+                            <a href="addMaterial.html" class="dropdown-item">חומר</a>
+                            <a href="addProject.php" class="dropdown-item">פרויקט</a>
+                            <a href="addException.php" class="dropdown-item">חריגה</a>
+                            <a href="addSupplier.html" class="dropdown-item">ספק</a>
+                            <a href="addVehicle.php" class="dropdown-item">רכב & ציוד צמ"ה</a>
+                            <a href="" class="dropdown-item">צ'יק</a>
+                            <a href="addReport.php" class="dropdown-item">דו"ח תנועה</a>
+                            <a href="addFuel.php" class="dropdown-item">דיווח בנזין</a>
+                            <a href="carFix.php" class="dropdown-item">טיפול רכב</a>
+                            
+                        </div>
+                    </div>
+                    <div class="nav-item dropdown">
+                        <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown"><i class="far fa-edit me-2"></i>עריכה & מחיקה</a>
+                        <div class="dropdown-menu bg-transparent border-0">
+                        <a href="editEmployee.php" class="dropdown-item">עובד</a>
+                            <a href="editClient.php" class="dropdown-item">לקוח</a>
+                            <a href="editMaterial.php" class="dropdown-item">חומר</a>
+                            <a href="" class="dropdown-item">פרויקט</a>
+                            <a href="" class="dropdown-item">משמרת</a>
+                            <a href="editException.php" class="dropdown-item">חריגה</a>
+                            <a href="editSupplier.php" class="dropdown-item">ספק</a>
+                            <a href="editCar.php" class="dropdown-item">רכב & ציוד צמ"ה</a>
+                            <a href="" class="dropdown-item">צ'יק</a>
+                            <a href="editReport.php" class="dropdown-item">דו"ח תנועה</a>
+                            <a href="editFuel.php" class="dropdown-item">דיווח בנזין</a>
+                            <a href="editFixing.php" class="dropdown-item">טיפול רכב</a>
+                        </div>
+                    </div>
+                   
+                    
+                </div>
+            </nav>
+        </div>
+        <!-- Sidebar End -->
+
+
+        <!-- Content Start -->
+        <div class="content">
+              <!-- Navbar Start -->
+              <nav class="navbar navbar-expand bg-light navbar-light sticky-top px-4 py-0">
+                <a href="index.html" class="navbar-brand d-flex d-lg-none me-4">
+                    <h2 class="text-primary mb-0"><i class="fa fa-hashtag"></i></h2>
+                </a>
+                <a href="#" class="sidebar-toggler flex-shrink-0">
+                    <i class="fa fa-bars"></i>
+                </a>
+                <form class="d-none d-md-flex ms-4">
+                    <input class="form-control border-0" type="search" placeholder="Search">
+                </form>
+                <div class="navbar-nav align-items-center ms-auto">
+                    <div class="nav-item dropdown">
+                        <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">
+                            <i class="fa fa-bell me-lg-2"></i>
+                            <span class="d-none d-lg-inline-flex">התראות</span>
+                        </a>
+                        <div class="dropdown-menu dropdown-menu-end bg-light border-0 rounded-0 rounded-bottom m-0">
+                            <a href="#" class="dropdown-item">
+                                <h6 class="fw-normal mb-0">נוסף עובד חדש</h6>
+                                <small>לפני 15 דקות</small>
+                            </a>
+                            <hr class="dropdown-divider">
+                            <a href="#" class="dropdown-item">
+                                <h6 class="fw-normal mb-0">משמרת עובד נקלטה</h6>
+                                <small>לפני 20 דקות</small>
+                            </a>
+                            <hr class="dropdown-divider">
+                            <a href="#" class="dropdown-item">
+                                <h6 class="fw-normal mb-0">הסיסמה שונתה</h6>
+                                <small>לפני 22 דקות</small>
+                            </a>
+                            <hr class="dropdown-divider">
+                            <a href="notifications.html" class="dropdown-item text-center">הצגת כל ההתראות</a>
+                        </div>
+                    </div>
+                    <div class="nav-item dropdown">
+                        <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">
+                            <img class="rounded-circle me-lg-2" src="img/user.jpg" alt="" style="width: 40px; height: 40px;">
+                            <span class="d-none d-lg-inline-flex">רפיק גבארין</span>
+                        </a>
+                        <div class="dropdown-menu dropdown-menu-end bg-light border-0 rounded-0 rounded-bottom m-0">
+                            <a href="profile.html" class="dropdown-item">הפרופיל שלי</a>
+                            <a href="index.html" class="dropdown-item">יציאה</a>
+                        </div>
+                    </div>
+                </div>
+            </nav>
+            <!-- Navbar End -->
+
+
+            <div class="col-sm-12 custom-form">
+    <div class="bg-light rounded p-4 custom-form-container" dir="rtl">      
+          <h5 class="mb-4">הוספת עובד</h5>
+                    <form  method="post" dir="rtl" >
+                        <div class="form-floating mb-3 position-relative">
+                            <input type="text" class="form-control" id="fullName" name="fullName" placeholder="">
+                            <label for="fullName" class="position-absolute top-0 end-0">שם מלא</label>
+                        </div>
+
+                        <div class="form-floating mb-3">
+                            <select class="form-select" id="job" name="job" aria-label="Floating label select example">
+                                <option selected>בחר/י</option>
+                                <option value="מנהל">מנהל</option>
+                                <option value="עובד">עובד</option>
+                                <option value="מזכירה">מזכירה</option>
+                            </select>
+                            <label for="job" class="position-absolute top-0 end-0">תפקיד</label>
+                        </div>
+                        <div class="form-floating mb-3">
+                            <input type="text" class="form-control" id="daySalary" name="daySalary" placeholder="">
+                            <label for="daySalary" class="position-absolute top-0 end-0">משכורת ליום</label>
+                        </div>
+                        <div class="form-floating mb-3">
+                            <input type="date" class="form-control" id="startDate" name="startDate" placeholder="">
+                            <label for="startDate" class="position-absolute top-0 end-0">תאריך התחלת עבודה</label>
+                        </div>
+                        <div class="form-floating mb-3">
+                            <select class="form-select" id="Gender" name="Gender" aria-label="Floating label select example">
+                                <option selected>בחר/י</option>
+                                <option value="זכר">זכר</option>
+                                <option value="נקבה">נקבה</option>
+                            </select>
+                            <label for="Gender" class="position-absolute top-0 end-0">מין</label>
+                        </div>
+                        
+                        <button class="btn btn-primary">הוסף</button>
+                        <!-- Display error message -->
+                        <?php if (isset($_SESSION["errorMessage"]) && !empty($_SESSION["errorMessage"])) { ?>
+                            <div class="alert alert-danger" role="alert">
+                                <?php echo $_SESSION["errorMessage"]; ?>
+                            </div>
+                        <?php } ?>
+
+                        <!-- Display success message -->
+                        <?php if (isset($_SESSION["successMessage"]) && !empty($_SESSION["successMessage"])) { ?>
+                            <div class="alert alert-success" role="alert">
+                                <?php echo $_SESSION["successMessage"]; ?>
+                            </div>
+                        <?php } ?>
+
+                        <!-- Clear session variables after displaying messages -->
+                        <?php
+                        unset($_SESSION["errorMessage"]);
+                        unset($_SESSION["successMessage"]);
+                        ?>
+                    </form>
+                   
+                    <!-- Clearing form inputs after refreshing the page submission -->
+                    <script>
+                        if ($successMessage !== ""){
+                            document.addEventListener("DOMContentLoaded", function() {
+                        document.getElementById("fullName").value = "";
+                        document.getElementById("job").value = "";
+                        document.getElementById("startDate").value = "";
+                        document.getElementById("Gender").value = "";
+                        document.getElementById("daySalary").value = "";
+                        document.getElementById("errorMessage").textContent = "";
+                        document.getElementById("successMessage").textContent = "";
+                    });
+
+                    }
+                </script>
+                </div>
+            </div>
+            
+
+
+            <!-- Footer Start -->
+            <div class="container-fluid pt-4 px-4">
+                <div class="bg-light rounded-top p-4">
+                    <div class="row">
+                        <div class="col-12 col-sm-6 text-center text-sm-start">
+                            &copy; <a href="#">Your Site Name</a>, All Right Reserved. 
+                        </div>
+                        <div class="col-12 col-sm-6 text-center text-sm-end">
+                            <!--/*** This template is free as long as you keep the footer author’s credit link/attribution link/backlink. If you'd like to use the template without the footer author’s credit link/attribution link/backlink, you can purchase the Credit Removal License from "https://htmlcodex.com/credit-removal". Thank you for your support. ***/-->
+                            Designed By <a href="https://htmlcodex.com">HTML Codex</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- Footer End -->
+        </div>
+        <!-- Content End -->
+
+
+        <!-- Back to Top -->
+        <a href="#" class="btn btn-lg btn-primary btn-lg-square back-to-top"><i class="bi bi-arrow-up"></i></a>
+    </div>
+
+    <!-- JavaScript Libraries -->
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="lib/chart/chart.min.js"></script>
+    <script src="lib/easing/easing.min.js"></script>
+    <script src="lib/waypoints/waypoints.min.js"></script>
+    <script src="lib/owlcarousel/owl.carousel.min.js"></script>
+    <script src="lib/tempusdominus/js/moment.min.js"></script>
+    <script src="lib/tempusdominus/js/moment-timezone.min.js"></script>
+    <script src="lib/tempusdominus/js/tempusdominus-bootstrap-4.min.js"></script>
+
+    <!-- Template Javascript -->
+    <script src="js/main.js"></script>
+</body>
+
+</html>
