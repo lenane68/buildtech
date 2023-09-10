7f0dc38266f4d54d2cb9aad8e75ffe123be3f056
Nermin <nrmeen.ra.123@gmail.com>
2023-09-10 22:12:23 +0300

    update database config

--- database.php
+++ database.php
@@ -1,7 +1,7 @@
 <?php
 
 // Database connection
-$conn = new mysqli('localhost','root','','buildtech');
+$conn = new mysqli('localhost','buildtechroot','Nrmeen123','buildtech');
 if($conn->connect_error){
     echo "$conn->connect_error";
     die("Connection Failed : ". $conn->connect_error);
